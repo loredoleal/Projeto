d9f73ba3332df8af207cc2e272590cf9e92166f0
631010160 <631010160@Sala801-06.educcbx.senacrs.com.br>
2012-11-01 09:31:47 -0200

    BD test

--- Projeto_LP2/src/temp.java
+++ Projeto_LP2/src/temp.java
@@ -1,4 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 
 public class temp {
+	public static void main(String[] args) throws Exception {
+		
+	    Connection con = DriverManager.getConnection(
+	                         "jdbc:postgresql://localhost:5432/LP2",
+	                         "postgres",
+	                         "senacrs");
 
+	    Statement stmt = con.createStatement();
+	    ResultSet rs = stmt.executeQuery("SELECT cod FROM \"Cliente\"");
+
+	    while (rs.next()) {
+	        int x = rs.getInt("cod");
+	        System.out.printf("%d\n", x);
+	    }
+	}
 }
