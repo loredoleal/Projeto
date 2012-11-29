9fbb8de771d8a117302d1f417a68b185a4bd7fbf
631010160 <631010160@Sala801-06.educcbx.senacrs.com.br>
2012-11-29 11:18:38 -0200

    the end begin

--- Projeto_LP2/src/swing/Servico.java
+++ Projeto_LP2/src/swing/Servico.java
@@ -1,5 +1,17 @@
 package swing;
 
+import java.awt.CardLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class Servico {
+	public static final String PRINCIPAL = "PRINCIPAL";
+	
+	private static void createAndShowGUI() {
+		JFrame frame = new JFrame("VTI Informática");
+		CardLayout cards = new CardLayout();
+		JPanel principal = new JPanel(cards);
+	}
 
 }
